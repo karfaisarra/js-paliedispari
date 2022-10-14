1039f94be4e6c040b9f353e08605f64a2930baab
karfaisarra <karfaisarra@gmail.com>
2022-10-14 14:29:02 +0200

    add palidroma function

--- Palidroma/assets/js/main.js
+++ Palidroma/assets/js/main.js
@@ -1,5 +1,48 @@
 /* Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma */
 
+//prova 1
+/*
+const parola = prompt('inserisci una parola');
+console.log(parola);
+const parolaDivisa = parola.split('');
+console.log(parolaDivisa);
+
+const parolaDivisaInvertita = parolaDivisa.reverse();
+console.log(parolaDivisaInvertita);
+
+
+const parolaInvertita = parolaDivisaInvertita.join('');
+console.log(parolaInvertita);
+
+if(parola == parolaInvertita){
+    console.log('la parola è palindroma');
+} else {
+    console.log('la parola non è palindroma');
+} 
+*/
+//prova 2
+
 // chiedo all'utente di inserire una parola
+const parola = prompt('inserisci una parola');
+console.log(parola);
+
 //faccio una funzione che fa un confronto tra una stringa letta da sinistra verso destra e della stessa stinga letta da destra verso sinistra usando if else 
-//stampo il resultato
+
+function palidroma(word) {
+    const parolaDivisa = word.split('');
+    //console.log(parolaDivisa);
+    const parolaDivisaInvertita = parolaDivisa.reverse();
+    //console.log(parolaDivisaInvertita);
+    const parolaInvertita = parolaDivisaInvertita.join('');
+    //console.log(parolaInvertita);
+    return parolaInvertita;
+}
+const risultato = palidroma(parola);
+//console.log('sono la nuova variabile che contiene il risultato della funzione', risultato );
+
+//stampo il risultato
+if(parola == risultato){
+    console.log('la parola è palindroma');
+} else {
+    console.log('la parola non è palindroma');
+} 
