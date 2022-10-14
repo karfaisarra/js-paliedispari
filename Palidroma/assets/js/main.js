8c701ca682f3476a10cf221b48150ccabf27b8dc
karfaisarra <karfaisarra@gmail.com>
2022-10-14 12:05:00 +0200

    add comments

--- Palidroma/assets/js/main.js
+++ Palidroma/assets/js/main.js
@@ -1,5 +1,5 @@
 /* Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma */
 
-// chiedo all'utente di inserire un numero
+// chiedo all'utente di inserire una parola
 //faccio una funzione che fa un confronto tra una stringa letta da sinistra verso destra e della stessa stinga letta da destra verso sinistra usando if else 
 //stampo il resultato
